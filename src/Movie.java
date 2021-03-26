4d582d33498bd2feaad0ae5768d8429080afd83f
najch <najch@DESKTOP-TTD3SIP>
2021-03-25 23:59:57 -0400

    Added Movie class information based on Class diagram.

--- src/Movie.java
+++ src/Movie.java
@@ -1,9 +1,58 @@
 
 public class Movie {
 
-	public static void main(String[] args) {
-		System.out.println("Testing");
+		//============================== Properties
+		String name;
+		String genre;
+		String director;
+		private String movieType;
+		int runtime;
 
-	}
+		//============================== Constructors
+		public Movie(String title, String genre, String director, int runtime) {
+			setName(name);
+			setGenre(genre);
+			setDirector(director);
+			setRuntime(runtime);
+		}
 
+		//============================== Methods
+		public String type() {
+			return movieType;
+		}
+
+		//============================== Getters/Setters
+		protected String getName() {
+			return name;
+		}
+
+		protected void setName(String name) {
+			this.name = name;
+		}
+
+		protected String getGenre() {
+			return genre;
+		}
+
+		protected void setGenre(String genre) {
+			this.genre = genre;
+		}
+
+		protected String getDirector() {
+			return director;
+		}
+
+		protected void setDirector(String director) {
+			this.director = director;
+		}
+
+		protected int getRuntime() {
+			return runtime;
+		}
+
+		protected void setRuntime(int runtime) {
+			this.runtime = runtime;
+		}
+
+		
 }
