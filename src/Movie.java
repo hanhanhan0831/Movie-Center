77e9b0da1aa21499e3d949d7c0630424472e00ae
starrijk <64489010+starrijk@users.noreply.github.com>
2021-04-07 08:58:35 -0400

    Added equals(Movie m) method to Movie.java. Changed movieType to year

--- src/Movie.java
+++ src/Movie.java
@@ -5,21 +5,18 @@ public class Movie {
 		private String name;
 		private String genre;
 		private String director;
-		private String movieType;
+		private int yearReleased;
 		private double runtime;
 
 		//============================== Constructors
-		protected Movie(String title, String genre, String director, double runtime) {
+		protected Movie(String title, String genre, String director, double runtime, int year) {
 			setName(title);
 			setGenre(genre);
 			setDirector(director);
 			setRuntime(runtime);
+			setYearReleased(year);
 		}
 
-		//============================== Methods
-		public String type() {
-			return movieType;
-		}
 
 		//============================== Getters/Setters
 		public String getName() {
@@ -54,21 +51,24 @@ public class Movie {
 			this.runtime = runtime;
 		}
 
-		public String getMovieType() {
-			return movieType;
+		public int getYearReleased() {
+			return yearReleased;
 		}
 
-		protected void setMovieType(String movieType) {
-			this.movieType = movieType;
+		protected void setYearReleased(int yearReleased) {
+			this.yearReleased = yearReleased;
 		}
 		
 		protected String forFile() {
-			return this.name+","+this.director+","+this.genre+","+this.runtime+","+this.movieType+"\n";
-		}
-		@Override
-		public String toString() {
-		// TODO Auto-generated method stub
-		return "" + getName() + ", " + getMovieType() + ", " + "Year Released";
+			return this.name+","+this.director+","+this.genre+","+this.runtime+","+this.yearReleased+"\n";
 		}
 		
+		@Override
+		public String toString() {
+		return "" + getName() + ", " + getYearReleased() + ", " + "Year Released";
+		}
+		
+		public boolean equals(Movie m) {
+			return this.forFile().equals(m.forFile());
+		}
 }
