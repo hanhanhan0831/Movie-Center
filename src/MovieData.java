12b5a34e57cc8fbc26f674122c9df85194d1e777
starrijk <64489010+starrijk@users.noreply.github.com>
2021-04-07 09:22:05 -0400

    Methods for stacking filters now take ArrayList<Movie> instead of Movie[]

--- src/MovieData.java
+++ src/MovieData.java
@@ -1,5 +1,7 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -26,6 +28,23 @@ public class MovieData {
 	}
 	
 	
+	protected static ArrayList<Movie>getAllMovies(){
+		ArrayList<Movie> mList = new ArrayList<>();
+		try {
+			Scanner reader = new Scanner(data);
+			while(reader.hasNextLine()) {
+				String line = reader.nextLine();
+				String[] pieces = line.split(",");
+				Movie m = new Movie(pieces[0], pieces[2], pieces[1], Double.parseDouble(pieces[3]), Integer.parseInt(pieces[4]));
+				mList.add(m);
+			}
+			reader.close();
+		}catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return mList;
+	}
+	
 	protected static Movie getMovieByTitle(String title) {
 		try {
 			Scanner reader = new Scanner(data);
@@ -33,8 +52,8 @@ public class MovieData {
 				String line = reader.nextLine();
 				String[] pieces = line.split(",");
 				if(pieces[0].equals(title)) {
-					//Index 0 is title, Index 1 is director, Index 2 is genre, Index 3 is runtime, Index 4 is type
-					Movie m = new Movie(pieces[0], pieces[2], pieces[1], Double.parseDouble(pieces[3]));
+					//Index 0 is title, Index 1 is director, Index 2 is genre, Index 3 is runtime, Index 4 is year
+					Movie m = new Movie(pieces[0], pieces[2], pieces[1], Double.parseDouble(pieces[3]), Integer.parseInt(pieces[4]));
 					reader.close();
 					return m;
 				}
@@ -43,7 +62,7 @@ public class MovieData {
 		}catch(FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		Movie m = new Movie("Movie Not Found", null, null, 0);
+		Movie m = new Movie("Movie Not Found", null, null, 0,0000);
 		return m;
 	}
 	
@@ -57,8 +76,8 @@ public class MovieData {
 				String line = reader.nextLine();
 				String[] pieces = line.split(",");
 				if(pieces[0].contains(search)) {
-					//Index 0 is title, Index 1 is director, Index 2 is genre, Index 3 is runtime, Index 4 is type
-					Movie m = new Movie(pieces[0], pieces[2], pieces[1], Double.parseDouble(pieces[3]));
+					//Index 0 is title, Index 1 is director, Index 2 is genre, Index 3 is runtime, Index 4 is year
+					Movie m = new Movie(pieces[0], pieces[2], pieces[1], Double.parseDouble(pieces[3]), Integer.parseInt(pieces[4]));
 					mList.add(m);
 				}
 			}
@@ -69,7 +88,7 @@ public class MovieData {
 		return mList;
 	}
 	
-	protected static ArrayList<Movie> getMoviesByTitle(String search, Movie[] movies){
+	protected static ArrayList<Movie> getMoviesByTitle(String search, ArrayList<Movie> movies){
 		ArrayList<Movie> mList = new ArrayList<>();
 		for(Movie m : movies) {
 			if(m.getName().contains(search)) {
@@ -88,8 +107,8 @@ public class MovieData {
 				String line = reader.nextLine();
 				String[] pieces = line.split(",");
 				if(director.equals(pieces[1])) {
-					//Index 0 is title, Index 1 is director, Index 2 is genre, Index 3 is runtime, Index 4 is type
-					Movie m = new Movie(pieces[0], pieces[2], pieces[1], Double.parseDouble(pieces[3]));
+					//Index 0 is title, Index 1 is director, Index 2 is genre, Index 3 is runtime, Index 4 is year
+					Movie m = new Movie(pieces[0], pieces[2], pieces[1], Double.parseDouble(pieces[3]), Integer.parseInt(pieces[4]));
 					mList.add(m);
 				}
 			}
@@ -101,7 +120,7 @@ public class MovieData {
 	}
 	
 	
-	protected static ArrayList<Movie> getMoviesByDirector(String director, Movie[] movies){
+	protected static ArrayList<Movie> getMoviesByDirector(String director, ArrayList<Movie> movies){
 		ArrayList<Movie> mList = new ArrayList<>();
 		for(Movie m : movies) {
 			if(m.getDirector().equals(director)) {
@@ -120,8 +139,8 @@ public class MovieData {
 				String line = reader.nextLine();
 				String[] pieces = line.split(",");
 				if(genre.equals(pieces[2])) {
-					//Index 0 is title, Index 1 is director, Index 2 is genre, Index 3 is runtime, Index 4 is type
-					Movie m = new Movie(pieces[0], pieces[2], pieces[1], Double.parseDouble(pieces[3]));
+					//Index 0 is title, Index 1 is director, Index 2 is genre, Index 3 is runtime, Index 4 is year
+					Movie m = new Movie(pieces[0], pieces[2], pieces[1], Double.parseDouble(pieces[3]), Integer.parseInt(pieces[4]));
 					mList.add(m);
 				}
 			}
@@ -134,7 +153,7 @@ public class MovieData {
 	
 	
 	
-	protected static ArrayList<Movie> getMoviesByGenre(String genre, Movie[] movies){
+	protected static ArrayList<Movie> getMoviesByGenre(String genre, ArrayList<Movie> movies){
 		ArrayList<Movie> mList = new ArrayList<>();
 		for(Movie m : movies) {
 			if(m.getGenre().equals(genre)) {
@@ -156,8 +175,8 @@ public class MovieData {
 				String[] pieces = line.split(",");
 				double run = Double.parseDouble(pieces[3]);
 				if(run<=max && run>=min) {
-					//Index 0 is title, Index 1 is director, Index 2 is genre, Index 3 is runtime, Index 4 is type
-					Movie m = new Movie(pieces[0], pieces[2], pieces[1], Double.parseDouble(pieces[3]));
+					//Index 0 is title, Index 1 is director, Index 2 is genre, Index 3 is runtime, Index 4 is year
+					Movie m = new Movie(pieces[0], pieces[2], pieces[1], Double.parseDouble(pieces[3]), Integer.parseInt(pieces[4]));
 					mList.add(m);
 				}
 			}
@@ -170,7 +189,7 @@ public class MovieData {
 	
 	
 	
-	protected static ArrayList<Movie> getMoviesByRuntime(double max, double min, Movie[] movies){
+	protected static ArrayList<Movie> getMoviesByRuntime(double max, double min, ArrayList<Movie> movies){
 		ArrayList<Movie> mList = new ArrayList<>();
 		for(Movie m : movies) {
 			if(m.getRuntime()<=max && m.getRuntime()>=min) {
@@ -182,16 +201,17 @@ public class MovieData {
 	
 	
 	
-	protected static ArrayList<Movie> getMoviesByType(String type) {
+	protected static ArrayList<Movie> getMoviesByYear(int max, int min) {
 		ArrayList<Movie> mList = new ArrayList<>();
 		try {
 			Scanner reader = new Scanner(data);
 			while(reader.hasNextLine()) {
 				String line = reader.nextLine();
 				String[] pieces = line.split(",");
-				if(type.equals(pieces[1])) {
-					//Index 0 is title, Index 1 is director, Index 2 is genre, Index 3 is runtime, Index 4 is type
-					Movie m = new Movie(pieces[0], pieces[2], pieces[1], Integer.parseInt(pieces[3]));
+				int year = Integer.parseInt(pieces[4]);
+				if(year>=min&&year<=max) {
+					//Index 0 is title, Index 1 is director, Index 2 is genre, Index 3 is runtime, Index 4 is year
+					Movie m = new Movie(pieces[0], pieces[2], pieces[1], Double.parseDouble(pieces[3]), Integer.parseInt(pieces[4]));
 					mList.add(m);
 				}
 			}
@@ -203,24 +223,43 @@ public class MovieData {
 	}
 	
 	
-	protected static ArrayList<Movie> getMoviesByType(String type, Movie[] movies){
+	protected static ArrayList<Movie> getMoviesByYear(int max, int min, ArrayList<Movie> movies){
 		ArrayList<Movie> mList = new ArrayList<>();
 		for(Movie m : movies) {
-			if(m.getMovieType().equals(type)) {
+			if(m.getYearReleased()<=max&&m.getYearReleased()>=min) {
 				mList.add(m);
 			}
 		}
 		return mList;
 	}
 	
+	protected static void removeMovieTitleYear(String title, int year) {
+		try {
+			StringBuffer sb = new StringBuffer("");
+			BufferedReader reader = new BufferedReader(new FileReader(data));
+			String line;
+			while((line=reader.readLine())!=null) {
+				String[] pieces = line.split(",");
+				if(!pieces[0].equals(title)||Integer.parseInt(pieces[4])!=year) {
+					sb.append(line);
+					sb.append("\n");
+				}
+			}
+			reader.close();
+			FileWriter writer=new FileWriter(data);
+			writer.write(sb.toString());
+			writer.close();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	
 	
 	public static void main(String[] args) {
-		//Movie m = new Movie("movieTitle", "movieGenre", "movieDirector", 10);
-		//storeMovie(m);
 		System.out.println(getMovieByTitle("movieTitle").forFile());
 		System.out.println(getMovieByTitle("not Movie").forFile());
-
+		removeMovieTitleYear("string", 1);
 	}
 
 }
